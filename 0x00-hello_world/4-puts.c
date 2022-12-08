04a45384cb7e0fdd917436530e5c7e09e59ee50b
Gideon Mba <abaaneg@gmail.com>
2022-12-08 05:18:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
+
+
+
+
+	/**
+	 *
+	 * *main - Entry point
+	 *
+	 * *Return: Always 0 (Success)
+	 *
+	 * */
+
+
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
