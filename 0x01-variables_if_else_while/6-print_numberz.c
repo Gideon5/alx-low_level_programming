4b30abeaee319ae6ce3c1181cf0c112c8d3d5b1c
Gideon Mba <abaaneg@gmail.com>
2022-12-09 08:31:55 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digi numbers of base 10 starting from 0
+ *
+ * Retrun: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 48; number < 598; number++)
+	{
+		putchar(number);
+	}
+	putchar("\n");
+	return (0);
+}
+
