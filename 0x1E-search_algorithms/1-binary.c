90f6dd4607ae6cd13174418c0a9c19108a706db5
Gideon Mba <abaaneg@gmail.com>
2023-10-11 16:26:37 -0700

    debugging

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,57 +1,35 @@
 #include "search_algos.h"
 
 /**
- * binary_search - binary search implementation
- * @array: string
- * @size: size_t
- * @value: value
- * Return: int, index of the value
+ * binary_search - linear search algorithm for arrays.
+ * @array: A pointer to inputed array.
+ * @size: The size of array.
+ * @value: The value to search for.
+ * Return: The index of the value into the array.
  */
-
 int binary_search(int *array, size_t size, int value)
 {
-size_t i;
-int left = 0;
-int right = size;
-int middle;
-if (array == NULL)
-	return (-1);
+	size_t i = 0, m = 0, l = 0, r = size - 1;
 
-for (i = 0; left < right; i++)
-{
-	printf("Searching in array: ");
-	print_array(array, left, right);
-	middle = (left + right) / 2;
-
-	if (array[middle] < value)
-		left = middle + 1;
-	else if (array[middle] > value)
-		right = middle;
-	else
-		return (middle);
-}
-return (-1);
-}
-
-
-/**
- * print_array - print `n` elements of an array of integers
- * @a: int array pointer to print
- * @i: int, start index
- * @j: int, end index
- * Description: Numbers must be separated by comma and space.
- * Numbers should be displayed in the same order they are stored in array.
- * You can only use _putchar to print.
- */
-
-void print_array(int *a, int i, int j)
-{
-
-	for (; i < j; i++)
+	if (!array || size <= 0)
+		return (-1);
+	while (l <= r)
 	{
-		printf("%d", a[i]);
-		if (i < j - 1)
-			printf(", ");
+		printf("Searching in array: ");
+		for (i = l; i <= r; i++)
+		{
+			if (i < r)
+				printf("%d, ", array[i]);
+			else
+				printf("%d\n", array[i]);
+		}
+		m = (l + r) / 2;
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			r = m - 1;
+		else
+			return (m);
 	}
-	printf("\n");
+	return (-1);
 }
